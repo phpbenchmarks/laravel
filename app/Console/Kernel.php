7728a334f6958610468b290fdd8472ba08011c9a
PHP Benchmarks <sbarboyon@free.fr>
2018-10-05 11:08:07 +0200

    Create Hello World benchmark

---
+++ app/Console/Kernel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    protected $commands = [];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')
+        //          ->hourly();
+    }
+
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+    }
+}
