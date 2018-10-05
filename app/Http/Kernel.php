502a011db132aec902799a9dfde9cffb79e53b5c
PHP Benchmarks <sbarboyon@free.fr>
2018-10-05 11:11:34 +0200

    Create REST API benchmark

---
+++ app/Http/Kernel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middlewareGroups = [
+        'web' => [],
+        'api' => [],
+    ];
+}
