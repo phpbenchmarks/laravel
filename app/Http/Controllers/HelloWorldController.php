be6e9d7fb5c7b046cb20a4b7c4ddcfe1a57aaca7
CHANOVE Tristan <pitchetrou@msn.com>
2020-04-24 17:42:20 +0200

    Cleanup + add benchmark-kit files

---
+++ app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HelloWorldController extends Controller
+{
+    public function helloWorld()
+    {
+        return 'Hello World !';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +11,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Illuminate\Support\Facades\Route::get('/benchmark/helloworld', 'HelloWorldController@helloWorld');
