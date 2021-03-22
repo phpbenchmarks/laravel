60e7c1fe60b8f3ebd25615b11b58a57278a1590c
CHANOVE Tristan <chanove.tristan@gmail.com>
2021-03-22 17:41:07 +0100

    Update latest laravel 7.x + php 8

--- app/Http/Controllers/RestApiController.php
+++ app/Http/Controllers/RestApiController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Events\DefineLang;
 use PhpBenchmarksRestData\Service;
+use Illuminate\Routing\Controller as BaseController;
 
-class RestApiController extends Controller
+class RestApiController extends BaseController
 {
     public function rest()
     {
